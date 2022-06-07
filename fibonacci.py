cfaccaab7f631cd8c2c5f458203921e839eb7222
mpsjunior <mpsjunior2017@gmail.com>
2022-06-07 15:08:03 -0300

    efetuado as correçoẽs, testes ok

--- fibonacci.py
+++ fibonacci.py
@@ -1,13 +1,16 @@
 n = int(input("Digite a quantidade de sequências: "))
 cont1 = 0
 cont2 = 1
-print("{} . {}" .format(cont1, cont2), end=" ")
-cont = 3
-while cont <= n:
+if (n == 1):
+   print(cont1)
+   exit()
+print("{} {}" .format(cont1, cont2), end=" ")
+
+while i in range(n-2):
     cont3 = cont1 + cont2
     print("{}".format(cont3), end=" ")
     cont1 = cont2 
     cont2 = cont3
-    cont += 1
+   
 print("fim")
     
