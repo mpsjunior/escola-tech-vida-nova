e5365d56cc20d557148fb83c7ef4fbdb8f322b96
mpsjunior <mpsjunior2017@gmail.com>
2022-06-07 14:16:50 -0300

    feito as modificações testes ok

--- FibonacciComFor.py
+++ FibonacciComFor.py
@@ -1,14 +1,15 @@
 n = int(input("Digite a quantidade de sequências: "))
 cont1 = 0
 cont2 = 1
-cont = 3
+if (n == 1):
+   print(cont1)
+   exit()
+print("{} {}".format(cont1, cont2), end= " ")
 
-for i in range(0, n):
+for i in range(n-2):
     cont3 = cont1 + cont2
     cont1 = cont2
     cont2 = cont3 
-    cont += 1
-
-print("O total é : {} ".format(cont3), end=" ")
+    print("{}".format(cont3), end=" ")
     
 print(" fim")
